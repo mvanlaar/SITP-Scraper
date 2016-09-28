0d4c775d503b125fc7c50397d8b0b55a2779aa1b
Martijn van Laar <martijn@mvanlaar.net>
2016-09-28 16:25:41 +0200

    Add parsign of route stations

--- SITP-Scraper/SITP-Scraper/Program.cs
+++ SITP-Scraper/SITP-Scraper/Program.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 
 
@@ -28,7 +29,9 @@ namespace SITP_Scraper
 
             List<Route> Rutas = new List<Route> { };
             List<Horario> Horarios = new List<Horario> { };
-            Console.WriteLine("Downloading files...")
+            List<RouteParada> RouteParadas = new List<RouteParada> { };
+            List<Parada> Paradas = new List<Parada> { };
+            Console.WriteLine("Downloading files...");
             foreach (string address in start_urls)
             {
                 HttpWebRequest request = WebRequest.Create(address) as HttpWebRequest;
@@ -52,7 +55,8 @@ namespace SITP_Scraper
                         rutaNombre = rutaNombre.Trim();
                         rutaNombre = HttpUtility.HtmlDecode(rutaNombre);
                         string rutaLink = row.SelectSingleNode(".//a[@class='rutaNombre']").Attributes["href"].Value.ToString();
-                        var urllink = new Uri(HttpUtility.HtmlDecode(rutaLink));
+                        rutaLink = HttpUtility.HtmlDecode(rutaLink);
+                        var urllink = new Uri(rutaLink);
                         string idRuta = HttpUtility.ParseQueryString(urllink.Query).Get("idRuta");
                         // List<string> horario = new List<string>();
                         Console.WriteLine("Parsing {0}", rutaNombre);
@@ -86,6 +90,99 @@ namespace SITP_Scraper
                 }
             // End url list parsing.
             }
+            // Looping through pages with Route information.
+            for (int i = 0; i < Rutas.Count; i++) // Loop through List with for)
+            {
+                Console.WriteLine("Parsing Route page: {0}", Rutas[i].rutaLink);
+                HttpWebRequest requestdetail = WebRequest.Create(Rutas[i].rutaLink) as HttpWebRequest;
+                requestdetail.Method = "GET";
+                using (HttpWebResponse responsedetail = requestdetail.GetResponse() as HttpWebResponse)
+                {
+                    HtmlDocument RutaDetail = new HtmlDocument();
+                    StreamReader readerdetail = new StreamReader(responsedetail.GetResponseStream());
+                    RutaDetail.LoadHtml(readerdetail.ReadToEnd());
+                    string savefile = String.Format("Download\\{0}-{1}.html", Rutas[i].tipoRuta, Rutas[i].idRuta);
+                    RutaDetail.Save(savefile);
+                    // Downloadable Files
+                    string linkMapaRuta = RutaDetail.DocumentNode.SelectSingleNode("//a[@class='linkMapaRuta']").Attributes["href"].Value.ToString();
+                    string linkPuntoParada = RutaDetail.DocumentNode.SelectSingleNode("//a[@class='linkPuntoParada']").Attributes["href"].Value.ToString();
+                    string linkPlegableRuta = RutaDetail.DocumentNode.SelectSingleNode("//a[@class='linkPlegableRuta']").Attributes["href"].Value.ToString();
+                    // Route Stations
+                    // Parsing Route information Direction A - Because a and b can be different.
+                    HtmlNode routea = RutaDetail.DocumentNode.SelectSingleNode("//div[@class='recorrido1']");
+                    {
+                        string direction = "A";
+                        foreach (HtmlNode NodeParada in routea.SelectNodes(".//div[@class='infoParada']"))
+                        { 
+                            string estNombre = NodeParada.SelectSingleNode(".//a").InnerHtml;
+                            estNombre = Regex.Replace(estNombre, @"<span class=""ignoreAaa"">.*?</span>", string.Empty);
+                            estNombre = HttpUtility.HtmlDecode(estNombre);
+                            estNombre = estNombre.Trim();
+                            string estNumbre = NodeParada.SelectSingleNode(".//span[@class='ignoreAaa']").InnerText;
+                            estNumbre = estNumbre.Replace(".", "").Trim();
+                            string estDireccion = NodeParada.SelectSingleNode(".//div[@class='estDireccion']").InnerText;
+                            string estLink = NodeParada.SelectSingleNode(".//a").Attributes["href"].Value.ToString();
+                            estLink = HttpUtility.HtmlDecode(estLink);
+                            var urllink = new Uri(estLink);
+                            string estId = HttpUtility.ParseQueryString(urllink.Query).Get("paradero");
+                            RouteParadas.Add(new RouteParada
+                            {
+                                idRuta = Rutas[i].idRuta,
+                                rutaDirection = direction,
+                                estNumber = estNumbre,
+                                estId = estId
+                            }
+                            );
+                            Paradas.Add(new Parada
+                            {                                
+                                estId = estId,
+                                estNombre = estNombre,
+                                estDireccion = estDireccion,
+                                estLink = estLink
+                            }
+                            );
+                        }
+                    }
+                    // Parsing Route information Direction B
+                    HtmlNode routeb = RutaDetail.DocumentNode.SelectSingleNode("//div[@class='recorrido2']");
+                    {
+                        string direction = "B";
+                        foreach (HtmlNode NodeParada in routea.SelectNodes(".//div[@class='infoParada']"))
+                        {
+                            string estNombre = NodeParada.SelectSingleNode(".//a").InnerHtml;
+                            estNombre = Regex.Replace(estNombre, @"<span class=""ignoreAaa"">.*?</span>", string.Empty);
+                            estNombre = HttpUtility.HtmlDecode(estNombre);
+                            estNombre = estNombre.Trim();
+                            string estNumbre = NodeParada.SelectSingleNode(".//span[@class='ignoreAaa']").InnerText;
+                            estNumbre = estNumbre.Replace(".", "").Trim();
+                            string estDireccion = NodeParada.SelectSingleNode(".//div[@class='estDireccion']").InnerText;
+                            string estLink = NodeParada.SelectSingleNode(".//a").Attributes["href"].Value.ToString();
+                            estLink = HttpUtility.HtmlDecode(estLink);
+                            var urllink = new Uri(estLink);
+                            string estId = HttpUtility.ParseQueryString(urllink.Query).Get("paradero");
+                            RouteParadas.Add(new RouteParada
+                            {
+                                idRuta = Rutas[i].idRuta,
+                                rutaDirection = direction,
+                                estNumber = estNumbre,
+                                estId = estId
+                            }
+                            );
+                            Paradas.Add(new Parada
+                            {
+                                estId = estId,
+                                estNombre = estNombre,
+                                estDireccion = estDireccion,
+                                estLink = estLink
+                            }
+                            );
+                        }
+                    }
+                }
+            }
+
+
+
             // Export to CSV.
             string exportroutesfile = ExportDir + "\\routes.txt";
             Console.WriteLine("Creating Export File routes.txt ...");
@@ -135,6 +232,58 @@ namespace SITP_Scraper
 
             }
 
+            string exportparadasfile = ExportDir + "\\paradas.txt";
+            Console.WriteLine("Creating Export File paradas.txt ...");
+            using (var exportparada = new StreamWriter(exportparadasfile))
+            {
+                // Route record
+                var csvroutes = new CsvWriter(exportparada);
+                csvroutes.Configuration.Delimiter = ",";
+                csvroutes.Configuration.Encoding = Encoding.UTF8;
+                csvroutes.Configuration.TrimFields = true;
+                // header 
+                csvroutes.WriteField("estId");
+                csvroutes.WriteField("estNombre");
+                csvroutes.WriteField("estDireccion");
+                csvroutes.WriteField("estLink");
+                csvroutes.NextRecord();
+                for (int i = 0; i < Paradas.Count; i++) // Loop through List with for)
+                {
+                    csvroutes.WriteField(Paradas[i].estId);
+                    csvroutes.WriteField(Paradas[i].estNombre);
+                    csvroutes.WriteField(Paradas[i].estDireccion);
+                    csvroutes.WriteField(Paradas[i].estLink);
+                    csvroutes.NextRecord();
+                }
+
+            }
+
+            string exportrouteparadasfile = ExportDir + "\\routeparada.txt";
+            Console.WriteLine("Creating Export File routeparadas.txt ...");
+            using (var exportrouteparada = new StreamWriter(exportrouteparadasfile))
+            {
+                // Route record
+                var csvroutes = new CsvWriter(exportrouteparada);
+                csvroutes.Configuration.Delimiter = ",";
+                csvroutes.Configuration.Encoding = Encoding.UTF8;
+                csvroutes.Configuration.TrimFields = true;
+                // header 
+                csvroutes.WriteField("idRuta");
+                csvroutes.WriteField("rutaDirection");
+                csvroutes.WriteField("estNumber");
+                csvroutes.WriteField("estId");
+                csvroutes.NextRecord();
+                for (int i = 0; i < RouteParadas.Count; i++) // Loop through List with for)
+                {
+                    csvroutes.WriteField(RouteParadas[i].idRuta);
+                    csvroutes.WriteField(RouteParadas[i].rutaDirection);
+                    csvroutes.WriteField(RouteParadas[i].estNumber);
+                    csvroutes.WriteField(RouteParadas[i].estId);
+                    csvroutes.NextRecord();
+                }
+
+            }
+
         }
     }
 
@@ -145,6 +294,23 @@ namespace SITP_Scraper
         public string rutaLink;
         public string idRuta;
         public string tipoRuta;
+    }
+
+    public class RouteParada
+    {        
+        public string idRuta;
+        public string rutaDirection;
+        public string estNumber;
+        public string estId;
+    }
+
+    public class Parada
+    {        
+        public string estId;
+        public string estNombre;
+        public string estDireccion;
+        public string estLink;
+
     }
 
     public class Horario
